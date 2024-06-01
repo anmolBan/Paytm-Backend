cba908424dd6b88eb946ed11949df40bcf3b8d42
Anmol Bansal <68399171+anmolBan@users.noreply.github.com>
2024-06-01 11:37:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/v1', mainRouter);
 
+app.get("/", (req, res) => {
+   res.status(200).json({
+       message: "Hello World"
+   }); 
+});
+
 app.listen(PORT, () => {
     console.log("Server is running on port", PORT);
 })
